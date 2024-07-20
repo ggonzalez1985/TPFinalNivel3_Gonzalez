d65959d02548b691854aca669e345ea313b8d777
Gabriel Gonzalez <gabrielgonzalez1985@gmail.com>
2024-07-19 23:21:36 -0300

    cambios sin mucha onda

--- Catalogo-Web/DetalleArticuloEditable.aspx
+++ Catalogo-Web/DetalleArticuloEditable.aspx
@@ -1,6 +1,93 @@
 ﻿<%@ Page Title="Edita Tu Articulo" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="DetalleArticuloEditable.aspx.cs" Inherits="Catalogo_Web.DetalleArticuloEditable" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    <h1>edita tu porqueria</h1>
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-4">
+                <br />
+                <label class="form-label" style="font-weight: bold; font-size: 18px; display: block; margin: 0 auto; text-align: center;">Datos asociados a al articulo</label>
+                <br />
+
+                <div>
+                    <label class="form-label" style="font-weight: bold;">Id:</label>
+                    <asp:TextBox ID="txtId" ReadOnly="true" Style="cursor: not-allowed; width: 100px;" class="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div>
+                    <label class="form-label" style="font-weight: bold;">Codigo:</label>
+                    <asp:TextBox ID="txtCodigo" CssClass="form-control" runat="server" ></asp:TextBox>
+                </div>
+
+                <div>
+                    <label class="form-label" style="font-weight: bold;">Nombre:</label>
+                    <asp:TextBox ID="txtNombre" class="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div>
+                    <label class="form-label" style="font-weight: bold;">Descripcion:</label>
+                    <asp:TextBox ID="txtDescripcion" TextMode="MultiLine" class="form-control" runat="server"></asp:TextBox>
+                </div>
+
+                <div style="display: flex; align-items: center;">
+                    <div style="flex: 1;">
+                        <label class="form-label" style="font-weight: bold;">Marca:</label>
+                        <asp:DropDownList ID="DdlMarca" CssClass="form-select" runat="server"></asp:DropDownList>
+                    </div>
+                    <asp:Button ID="btnAgregarMarca" Text="+" class="btn btn-primary" runat="server" Style="margin-left: 20px; margin-top: 30px;" />
+                </div>
+
+                <div style="display: flex; align-items: center;">
+                    <div style="flex: 1;">
+                        <label class="form-label" style="font-weight: bold;">Categoria:</label>
+                        <asp:DropDownList ID="DdlCategoria" CssClass="form-select" runat="server"></asp:DropDownList>
+                    </div>
+                    <asp:Button ID="btnAgregarCategoria" Text="+" class="btn btn-primary" runat="server" Style="margin-left: 20px; margin-top: 30px;" />
+                </div>
+
+                <div>
+                    <label class="form-label" style="font-weight: bold;">Precio:</label>
+                    <asp:TextBox ID="TextBox1" class="form-control" runat="server" Style="width: 50%;"></asp:TextBox>
+                </div>
+
+
+
+
+                <br />
+                <div>
+                    <asp:Button ID="btnModificar" Text="Cambiar datos personales" class="btn btn-primary" runat="server" />
+                </div>
+            </div>
+
+
+
+
+
+            <div class="col-4">
+
+
+                <div class="mb-3">
+                    <label class="form-label">Imagen del articulo</label>
+                    <input type="file" id="txtImagen" runat="server" class="form-control" onchange="previewImage(this);" />
+                </div>
+
+                <asp:Image ID="imgArticulo" ImageUrl="https://www.palomacornejo.com/wp-content/uploads/2021/08/no-image.jpg"
+                    runat="server" CssClass="img-fluid mb-3" />
+
+                <div>
+                    <br />
+                    <asp:Button ID="btnCambiarImagen" Text="Cambiar imagen" class="btn btn-primary" runat="server" />
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
 </asp:Content>
--- Catalogo-Web/DetalleArticuloEditable.aspx.designer.cs
+++ Catalogo-Web/DetalleArticuloEditable.aspx.designer.cs
@@ -13,5 +13,122 @@ namespace Catalogo_Web
 
     public partial class DetalleArticuloEditable
     {
+
+        /// <summary>
+        /// Control txtId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtId;
+
+        /// <summary>
+        /// Control txtCodigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigo;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control DdlMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlMarca;
+
+        /// <summary>
+        /// Control btnAgregarMarca.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarMarca;
+
+        /// <summary>
+        /// Control DdlCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlCategoria;
+
+        /// <summary>
+        /// Control btnAgregarCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarCategoria;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+
+        /// <summary>
+        /// Control txtImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile txtImagen;
+
+        /// <summary>
+        /// Control imgArticulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgArticulo;
+
+        /// <summary>
+        /// Control btnCambiarImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCambiarImagen;
     }
 }
